002dd612e57eb2da1173011a7bb7637c26aef93b
Sebastian Birk <sebastian.birk@gmail.com>
2024-03-19 23:10:52 +0100

    Initial commit of Rust Project Euler Suite and solution to problem #44

---
+++ rust_euler_suite/build.rs
@@ -0,0 +1,8 @@
+//use std::env;  
+//use copy_to_output::copy_to_output;  
+
+fn main() {
+//    // Re-runs script if any files in res are changed  
+//    println!("cargo:rerun-if-changed=data/p042_words.txt");  
+//    copy_to_output("data/p042_words.txt", &env::var("PROFILE").unwrap()).expect("Could not copy");  
+}
---
+++ rust_euler_suite/number_sequences/src/pentagonal.rs
@@ -0,0 +1,102 @@
+use std::fmt;
+
+#[derive(PartialEq)]
+#[derive(Debug, Clone, Copy)]
+pub struct Pentagonal {
+    pub n: u32,
+    pub value: u32,
+}
+
+impl Pentagonal {
+    pub fn new(n: u32) -> Self {
+        Pentagonal {
+            n: n, 
+            value: n*(3*n-1)/2
+        }
+    }
+
+    pub fn prev(&self) -> Pentagonal {
+        Pentagonal {
+            n: self.n-1, 
+            value: self.value - (3*self.n-2)
+        }
+    }
+
+    pub fn next(&self) -> Pentagonal {
+        Pentagonal {
+            n: self.n+1, 
+            value: self.value + 3*self.n+1
+        }
+    }
+}
+
+impl fmt::Display for Pentagonal {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.n, self.value)
+    }
+}
+
+pub fn is_pentagonal(value: u32) -> Option<Pentagonal> {
+    is_pentagonal_with_hint(value, &Pentagonal::new(1))
+}
+
+pub fn is_pentagonal_with_hint(value: u32, hint: &Pentagonal) -> Option<Pentagonal> {
+    if value == hint.value {
+        Some(hint.clone())
+    }
+    else if value < hint.value {
+        if value <= hint.prev().value {
+            is_pentagonal_with_hint(value, &hint.prev())
+        }
+        else {
+            None
+        }
+    }
+    else {
+        if value >= hint.next().value {
+            is_pentagonal_with_hint(value, &hint.next())
+        }
+        else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let pent = Pentagonal::new(1);
+        assert_eq!(pent.n, 1);
+        assert_eq!(pent.value, 1);
+        let pent = Pentagonal::new(5);
+        assert_eq!(pent.n, 5);
+        assert_eq!(pent.value, 35);
+    }
+
+    #[test]
+    fn test_next() {
+        let pent = Pentagonal::new(4).next();
+        assert_eq!(pent.n, 5);
+        assert_eq!(pent.value, 35);
+    }
+
+    #[test]
+    fn test_prev() {
+        let pent = Pentagonal::new(6).prev();
+        assert_eq!(pent.n, 5);
+        assert_eq!(pent.value, 35);
+    }
+
+    #[test]
+    fn test_is_pentagonal() {
+        let pent = Pentagonal::new(6);
+        assert_eq!(is_pentagonal(pent.value).unwrap(), pent);
+        assert_ne!(is_pentagonal(5).unwrap(), pent);
+        assert_eq!(is_pentagonal(3), None);
+        assert_ne!(is_pentagonal_with_hint(5, &pent), None);
+        assert_eq!(is_pentagonal_with_hint(145, &pent), Some(Pentagonal::new(10)));
+    }
+}
